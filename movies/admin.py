018db41fb6dd5d80728c62a80e5719f1a0aff810
Fatih Erikli <fatiherikli@gmail.com>
2015-08-19 19:38:51 +0300

    Add admin app

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Movie, Person, Character, Genre
 
 # Register your models here.
+
+admin.site.register(Movie)
+admin.site.register(Person)
+admin.site.register(Character)
+admin.site.register(Genre)
--- movies/models.py
+++ movies/models.py
@@ -2,31 +2,33 @@ from django.db import models
 from django.conf import settings
 
 
-# Create your models here.
 class Movie(models.Model):
     title = models.CharField(max_length=255)
-    # Charfield belli bir uzunluktan kucuk stringleri tutar
+    # CharField belli bir uzunluktan kucuk stringleri tutar
     description = models.TextField(blank=True)
     # TextField sinirsiz uzunluktaki stringleri tutar
     # blank=True olan field bos birakilabilir demek
-    release_date = models.DateField()
+    release_date = models.DateField(null=True, blank=True)
     # DateField sadece ay yil ve gun tutar
-    director = models.ForeignKey('Person', related_name='movies')
+    director = models.ForeignKey('Person', related_name='directed_movies')
     # ForeignKey baska bir model ile baglanti kurar
     # related_name bu baglanti tersten kullanilirken degisken adina atanir
     # movie_set yerine movies dememiz yeterli olacak artik
-    total_score = models.PositiveIntegerField()
+    total_score = models.PositiveIntegerField(default=0)
     # PositiveIntegerField veritabaninda pozitif tam sayi tutar
-    genres = models.ManyToManyField('Genre', related_name='movies')
-    scored_users = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    genres = models.ManyToManyField('Genre', related_name='movies', blank=True)
+    scored_users = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True)
     # ManyToManyField bir model ile baska bir modeli coka cok sekilde baglar
-    # image = models.ImageField(null=True)
-    # ImageField bir resmin diskte tutulan yerini saklar
-    characters = models.ManyToManyField("Character", related_name="movies")
+    image = models.URLField(blank=True)
+    # URLField bir url saklamak icin kul1lanilir
+    characters = models.ManyToManyField("Character", related_name="movies", blank=True)
+
+    def __str__(self):
+        return self.title
 
 
 class Person(models.Model):
-    GENDERS =[
+    GENDERS = [
         ('m', "Male"),
         ('f', "Female"),
         ('o', "Other"),
@@ -34,19 +36,28 @@ class Person(models.Model):
 
     name = models.CharField(max_length=255)
     date_of_birth = models.DateField()
-    date_of_death = models.DateField(blank=True)
+    date_of_death = models.DateField(null=True, blank=True)
     gender = models.CharField(max_length=1, choices=GENDERS)
     # choices 'a atanan liste veritabanina kaydedilebilecek degerleri sinirlar
-    is_awarded = models.BooleanField()
+    is_awarded = models.BooleanField(default=False)
     # BooleanField true/false degerleri saklamak icin kullanilir
+
+    def __str__(self):
+        return self.name
 
 
 class Character(models.Model):
     person = models.ForeignKey(Person, related_name="characters")
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class Genre(models.Model):
     name = models.CharField(max_length=255)
-    parent = models.ForeignKey('self', null=True, related_name="sub_genres")
+    parent = models.ForeignKey('self', blank=True, null=True, related_name="sub_genres")
     # ForeignKey'deki self model'in kendisini ifade eder
+
+    def __str__(self):
+        return self.name
